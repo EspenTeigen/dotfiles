16fce8646fc955704ea98661affacfb6bff9cda3
Espen Teigen <espen.teigen@hotmail.com>
2025-09-08 21:41:19 +0200

    added zsh theme

---
+++ catppuccin-mocha/.catppuccin-mocha.zsh
@@ -0,0 +1,40 @@
+# Catppuccin Mocha color scheme for ZSH
+# https://github.com/catppuccin/catppuccin
+
+# Catppuccin Mocha colors
+export CATPPUCCIN_ROSEWATER="#f5e0dc"
+export CATPPUCCIN_FLAMINGO="#f2cdcd"
+export CATPPUCCIN_PINK="#f5c2e7"
+export CATPPUCCIN_MAUVE="#cba6f7"
+export CATPPUCCIN_RED="#f38ba8"
+export CATPPUCCIN_MAROON="#eba0ac"
+export CATPPUCCIN_PEACH="#fab387"
+export CATPPUCCIN_YELLOW="#f9e2af"
+export CATPPUCCIN_GREEN="#a6e3a1"
+export CATPPUCCIN_TEAL="#94e2d5"
+export CATPPUCCIN_SKY="#89dceb"
+export CATPPUCCIN_SAPPHIRE="#74c7ec"
+export CATPPUCCIN_BLUE="#89b4fa"
+export CATPPUCCIN_LAVENDER="#b4befe"
+export CATPPUCCIN_TEXT="#cdd6f4"
+export CATPPUCCIN_SUBTEXT1="#bac2de"
+export CATPPUCCIN_SUBTEXT0="#a6adc8"
+export CATPPUCCIN_OVERLAY2="#9399b2"
+export CATPPUCCIN_OVERLAY1="#7f849c"
+export CATPPUCCIN_OVERLAY0="#6c7086"
+export CATPPUCCIN_SURFACE2="#585b70"
+export CATPPUCCIN_SURFACE1="#45475a"
+export CATPPUCCIN_SURFACE0="#313244"
+export CATPPUCCIN_BASE="#1e1e2e"
+export CATPPUCCIN_MANTLE="#181825"
+export CATPPUCCIN_CRUST="#11111b"
+
+# Custom LS colors for Catppuccin
+export LS_COLORS="rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:"
+
+# Catppuccin colors for various tools
+export CATPPUCCIN_FZF_OPTS="
+--color=bg+:$CATPPUCCIN_SURFACE0,bg:$CATPPUCCIN_BASE,spinner:$CATPPUCCIN_ROSEWATER
+--color=hl:$CATPPUCCIN_RED,fg:$CATPPUCCIN_TEXT,header:$CATPPUCCIN_RED
+--color=info:$CATPPUCCIN_MAUVE,pointer:$CATPPUCCIN_ROSEWATER,marker:$CATPPUCCIN_ROSEWATER
+--color=fg+:$CATPPUCCIN_TEXT,prompt:$CATPPUCCIN_MAUVE,hl+:$CATPPUCCIN_RED"
