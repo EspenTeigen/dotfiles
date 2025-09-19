68eead1a005956d5619175418a37de0acc535474
Espen Teigen <espen.teigen@hotmail.com>
2025-09-19 13:57:37 +0200

    added folding

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,11 +3,23 @@ return {
   opts = {
     formatters_by_ft = {
       xml = { "xmllint" },
+      text = { "fold" },
+      markdown = { "prettier" },
     },
     formatters = {
       xmllint = {
         command = "xmllint",
         args = { "--format", "-" },
+        stdin = true,
+      },
+      fold = {
+        command = "fold",
+        args = { "-s", "-w", "80" },
+        stdin = true,
+      },
+      prettier = {
+        command = "prettier",
+        args = { "--prose-wrap", "always", "--print-width", "80", "--stdin-filepath", "$FILENAME" },
         stdin = true,
       },
     },
