432a1336c6f2b71b164235b472911ea79d1a3523
Espen <espen.teigen@ispas.no>
2025-10-14 08:43:31 +0200

    updateds to lsp and sway

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,18 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic",
+                diagnosticSeverityOverrides = {
+                  reportAbstractUsage = "error",
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
